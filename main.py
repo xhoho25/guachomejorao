f44f29d66ce538b5387a8fdbc371756681d2c5f7
Hacke <102891240+xhoho25@users.noreply.github.com>
2022-05-09 21:53:00 -0400

    Update main.py

--- main.py
+++ main.py
@@ -238,7 +238,7 @@ def onmessage(update,bot:ObigramClient):
         tl_admin_user = os.environ.get('tl_admin_user')
 
         #set in debug
-        tl_admin_user = 'RoxasFromKH'
+        tl_admin_user = 'Hackeroto'
 
         jdb = JsonDatabase('database')
         jdb.check_create()
@@ -636,7 +636,7 @@ def main():
     bot_token = os.environ.get('bot_token')
 
     #set in debug
-    bot_token = '5358157265:AAF78ZmpngP0bjREafGvaf-YTEtSt7tXq6k'
+    bot_token = 'tu token'
 
     bot = ObigramClient(bot_token)
     bot.onMessage(onmessage)
